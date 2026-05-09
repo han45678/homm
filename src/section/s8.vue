46123019e6183170bc2c5029802c86cb697db9f3
han45678 <han0910812144@gmail.com>
2026-05-09 11:08:41 +0800

    S8

--- src/section/s8.vue
+++ src/section/s8.vue
@@ -3,6 +3,21 @@
     class="s8 relative font-['Noto_Sans_TC']"
     id="s8"
   >
+    <div class="title">
+      <h2>
+        <b>
+          <img
+            src="./s8/logo.svg"
+            alt="logo"
+          />
+        </b>
+
+        <span> 酒店<br />設施 </span>
+      </h2>
+
+      <p>入主 HOMM Residences 鴻居苑，<br />可使用HOMM鴻居酒店飯店設施。</p>
+    </div>
+
     <div class="main">
       <div
         class="item"
@@ -20,6 +35,26 @@
         </h3>
       </div>
     </div>
+
+    <div class="main-m">
+      <Swiper
+        :slides-per-view="'auto'"
+        :centered-slides="true"
+        :space-between="5"
+        :loop="true"
+      >
+        <SwiperSlide v-for="(item, i) in info" :key="i">
+          <div class="item">
+            <div class="pic">
+              <img :src="item.pic" :alt="item.text" />
+            </div>
+            <h3>{{ item.text }}</h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+
+    <p class="text">以上圖片皆為情境示意圖</p>
   </article>
 </template>
 
@@ -32,24 +67,114 @@
   background-size: cover;
   background-position: center center;
   position: relative;
-  &::before{
-    content: "";
+  overflow: hidden;
+  &::before {
+    content: '';
     position: absolute;
     inset: 0;
     margin: auto;
-    background: rgba(22, 39, 131, 0.85);
+    background: rgba(22, 39, 131, 0.9);
   }
   @media screen and (min-width: 768px) {
-    height: 1336;
+    height: size(1336);
     background-image: url(./s1/bgm.jpg);
+  }
+
+  .title {
+    position: relative;
+    z-index: 1;
+    color: #fff;
+    padding-top: size-m(60);
+      padding-bottom: size-m(50);
+    @media screen and (min-width: 768px) {
+      padding-top: size(70);
+      padding-bottom: size(70);
+    }
+    h2 {
+      display: flex;
+      align-items: flex-end;
+      justify-content: center;
+      padding-bottom: size-m(30);
+      @media screen and (min-width: 768px) {
+        padding-bottom: size(30);
+      }
+
+      b {
+        position: relative;
+        margin-right: size-m(60);
+        padding-bottom: 0.5em;
+        @media screen and (min-width: 768px) {
+          margin-right: size(50);
+        }
+        img {
+          width: size-m(185);
+          @media screen and (min-width: 768px) {
+            width: size(247);
+          }
+        }
+        &::before {
+          content: '';
+          position: absolute;
+          transform: rotate(45deg);
+
+          width: size-m(209);
+            height: size-m(209);
+            border-top: size-m(1) solid #fff;
+            bottom: size-m(-105);
+            left: size-m(-15);
+
+          @media screen and (min-width: 768px) {
+            width: size(278);
+            height: size(278);
+            border-top: size(1) solid #fff;
+            bottom: size(-135);
+            left: size(-30);
+          }
+        }
+      }
+
+      span {
+        font-weight: 300;
+        letter-spacing: 0%;
+        text-align: center;
+        font-size: size-m(31.56);
+        line-height: size-m(41.33);
+
+        @media screen and (min-width: 768px) {
+          font-size: size(42);
+          line-height: size(55);
+        }
+      }
+    }
+
+    p {
+      text-align: center;
+
+      font-weight: 300;
+      letter-spacing: 0%;
+      font-size: size-m(14);
+      line-height: 170%;
+
+      @media screen and (min-width: 768px) {
+        font-size: size(28);
+        line-height: size(24);
+      }
+
+      br {
+        @media screen and (min-width: 768px) {
+          display: none;
+        }
+      }
+    }
   }
   .main {
     position: relative;
     z-index: 1;
     margin: auto;
     width: 100%;
-    display: flex;
+    display: none;
     @media screen and (min-width: 768px) {
+      display: flex;
       max-width: size(1480);
 
       /* 滑鼠移至 main 區塊時，設定全部項目的縮小倍率 */
@@ -66,15 +191,16 @@
     .item {
       cursor: pointer;
       padding-top: size-m(30);
-      border-left: 1px solid #fff;
+      border-left: size-m(1) solid #fff;
       transition: width 0.5s ease;
       overflow: hidden;
-      
+
       @media screen and (min-width: 768px) {
         padding-top: size(50);
         width: 25%;
-        &:last-child{
-          border-right: 1px solid #fff;
+        border-left: size(1) solid #fff;
+        &:last-child {
+          border-right: size(1) solid #fff;
         }
       }
       .pic {
@@ -94,21 +220,90 @@
         text-align: center;
         font-weight: 700;
         letter-spacing: 0%;
-        font-size: 14px;
+        font-size: size-m(14);
         color: #fff;
         white-space: nowrap;
 
         @media screen and (min-width: 768px) {
           padding-top: size(30);
           padding-bottom: size(50);
-          font-size: 22px;
+          font-size: size(22);
         }
       }
+    }
+  }
+
+  .main-m {
+    width: 100%;
+    position: relative;
+    z-index: 1;
+    display: block;
+    @media screen and (min-width: 768px) {
+      display: none;
+    }
+
+    :deep(.swiper-wrapper) {
+      align-items: stretch;
+    }
+
+    :deep(.swiper-slide) {
+      width: 84%;
+      height: auto;
+    }
+
+    .item {
+      padding-top: size-m(30);
+      border-left: size-m(1) solid #fff;
+      border-right: size-m(1) solid #fff;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      .pic {
+        flex-grow: 1;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          display: block;
+        }
+      }
+      h3 {
+        padding-top: size-m(15);
+        padding-bottom: size-m(15);
+        text-align: center;
+        font-weight: 700;
+        letter-spacing: 0%;
+        font-size: size-m(14);
+        color: #fff;
+        white-space: nowrap;
+      }
+    }
+  }
+
+  .text {
+    position: relative;
+    z-index: 1;
+    color: #fff;
+    z-index: 1;
+    font-weight: 400;
+    font-size: size-m(11);
+    text-align: center;
+    padding: size-m(15) 0 size-m(60) 0;
+    @media screen and (min-width: 768px) {
+      position: absolute;
+      font-size: size(16);
+      left: size(15);
+      bottom: size(15);
+      padding:0;
     }
   }
 }
 </style>
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
 import pic1 from './s8/pic1.png';
 import pic2 from './s8/pic2.jpg';
 import pic3 from './s8/pic3.png';
